cac2198dde5756c1984fb19f0a6dd7bc05d4d735
Raman1121 <happy07.dutt@gmail.com>
2022-02-13 19:12:21 +0000

    Code cleanup

--- src/run.py
+++ src/run.py
@@ -29,12 +29,6 @@ from model import model
 import yaml
 from pprint import pprint
 
-# parser = argparse.ArgumentParser(description='Hyper-parameters management')
-
-# parser.add_argument('--num_samples', type=int, default=5, help='Number of samples in a dataset')
-# parser.add_argument('--batch_size', type=int, default=64, help='Batch Size')
-# parser.add_argument('--num_aug_samples', type=int, default=10, help='Number of times you want to sample an augmentation')
-# parser.add_argument('--encoder', type=str, default='resnet50_supervised', help='Encoder Model')
 
 with open('config_train.yaml') as file:
     yaml_data = yaml.safe_load(file)
