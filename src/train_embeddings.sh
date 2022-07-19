dbd18d0815c6b3ef4e10e8c7aef574a09da271c5
Raman Dutt <co-dutt1@login-e-4.data.cluster>
2022-07-19 19:15:10 +0100

    bash file for integrated model updated

--- src/train_embeddings.sh
+++ src/train_embeddings.sh
@@ -4,7 +4,12 @@
 #SBATCH --partition ampere
 #SBATCH --gres=gpu:1
 #SBATCH --account BMAI-CDT-SL2-GPU
-#SBATCH --time=10:00:00
+#SBATCH --time=36:00:00
 
-python train_integrated.py --dataset chexpert --model dorsal 
+cd /home/co-dutt1/rds/hpc-work/SSL-Augmentations/src
+module load cuda/11.2
+module load cudnn/8.1_cuda-11.2
+source ~/rds/hpc-work/miniconda3/bin/activate mlp
+
+python train_integrated.py --dataset cancer_mnist --model dorsal 
 
